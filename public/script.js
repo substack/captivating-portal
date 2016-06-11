781bd87e0cad98f2c3d67833a8899c1283c89292
James Halliday <mail@substack.net>
2016-06-11 11:17:14 -0700

    fix localstorage

--- public/script.js
+++ public/script.js
@@ -4,5 +4,8 @@ var req = new XMLHttpRequest
 req.open('GET', '/count.txt', false)
 req.send(null)
 
-count.textContent = req.responseText
-localStorage.setItem('count', count.textContent)
+if (req.status === 200) {
+  count.textContent = req.responseText
+  console.log('SET', req.responseText)
+  localStorage.setItem('count', req.responseText)
+}
