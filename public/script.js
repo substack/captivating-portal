f210667b7d7c9cef41101cdd37b24563712e9df5
James Halliday <mail@substack.net>
2016-06-11 11:03:22 -0700

    capitvating portal

---
+++ public/script.js
@@ -0,0 +1,8 @@
+count.textContent = localStorage.getItem('count') || '----------'
+
+var req = new XMLHttpRequest
+req.open('GET', '/count.txt', false)
+req.send(null)
+
+count.textContent = req.responseText
+localStorage.setItem('count', count.textContent)
---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http')
+var ecstatic = require('ecstatic')
+var fs = require('fs')
+var count = 0
+var countFile = __dirname + '/public/count.txt'
+try { count = Number(fs.readFileSync(countFile)) }
+catch (err) {}
+if (isNaN(count)) count = 0
+count = Math.floor(count)
+
+var st = ecstatic({
+  root: __dirname + '/public',
+  cache: 9999999999999999999
+})
+http.createServer(function (req, res) {
+  if (req.url === '/') {
+    fs.writeFile(countFile, String(++count), function (err) {
+      st(req, res)
+    })
+  } else st(req, res)
+}).listen(Number(process.argv[2]) || 80)
